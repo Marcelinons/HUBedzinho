c9c625aded368499be2a00c9d662bc6363b46dcc
Marcelinons <76798076+Marcelinons@users.noreply.github.com>
2021-10-19 19:38:56 -0300

    Update ajax-utils.js

--- Hubed/HUBed/js/ajax-utils.js
+++ Hubed/HUBed/js/ajax-utils.js
@@ -4,31 +4,42 @@
   function getRequestObject() {
     if (window.XMLHttpRequest) {
       return (new XMLHttpRequest());	
-    } else {
+    } 
+    else if (window.ActiveXObject) {
+      // For very old IE browsers (optional)
+      return (new ActiveXObject("Microsoft.XMLHTTP"));
+    } 
+    else {
       global.alert("Ajax is not supported.");
       return (null);
     }
   }
 
-  // Makes Ajax GET request to 'requestUrl'
   ajaxUtils.sendGetRequest = 
-    function (requestUrl, responseHandler, isJson) {
-      var request = getRequestObject();
-      request.onreadystatechange = 
-        function () {
-          handleResponse(request, responseHandler, isJson);
-        };
-      request.open("GET", requestUrl, true);
-    };
+  function(requestUrl, responseHandler, isJsonResponse) {
+    var request = getRequestObject();
+    request.onreadystatechange = 
+      function() { 
+        handleResponse(request, 
+                       responseHandler,
+                       isJsonResponse); 
+      };
+    request.open("GET", requestUrl, true);
+    request.send(null); // for POST only
+  };
 
   function handleResponse(request, responseHandler, isJson) {
-    if ((request.readyState == 4) && (request.status == 200)) {
+    if ((request.readyState == 4) &&
+        (request.status == 200)) {
+      
       if (isJson == undefined) {
         isJson = true;
       }
+      
       if (isJson) {
         responseHandler(JSON.parse(request.responseText));
-      } else {
+      } 
+      else {
         responseHandler(request.responseText);
       }
     }
